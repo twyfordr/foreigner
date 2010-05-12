da69428ffa4074d50029dc86b18f6ff42446bd26
Marnen Laibow-Koser <mlaibow@ml-ny-mlaibow.amexpub.com>
2010-05-12 14:01:24 -0400

    Add more options.

--- lib/foreigner/connection_adapters/sqlserver_adapter.rb
+++ lib/foreigner/connection_adapters/sqlserver_adapter.rb
@@ -14,13 +14,11 @@ module Foreigner
       end
 
       def foreign_keys(table_name)
-        options = {}
-        
         fk_info = select_all %Q{
-          SELECT cs_o.name as 'name', cs_col.name as 'column', ref_o.name as 'to_table', ref_col.name as 'primary_key'
+          SELECT fk.name as 'name', cs_col.name as 'column', ref_o.name as 'to_table', ref_col.name as 'primary_key', fk.delete_referential_action_desc as 'delete_action'
           FROM sys.tables as t
             LEFT JOIN sys.foreign_key_columns as fkc ON (fkc.parent_object_id = t.object_id)
-            LEFT JOIN sys.objects as cs_o ON (cs_o.object_id = fkc.constraint_object_id)
+            LEFT JOIN sys.foreign_keys as fk ON (fk.object_id = fkc.constraint_object_id)
             LEFT JOIN sys.columns as cs_col ON (cs_col.object_id = fkc.parent_object_id AND cs_col.column_id = fkc.parent_column_id)
             LEFT JOIN sys.objects as ref_o ON (ref_o.object_id = fkc.referenced_object_id)
             LEFT JOIN sys.columns as ref_col ON (ref_col.object_id = fkc.referenced_object_id AND ref_col.column_id = fkc.referenced_column_id)
@@ -28,6 +26,17 @@ module Foreigner
         }
 
         fk_info.collect do |row|
+          options = {:column => row['column'], :name => row['name'], :primary_key => row['primary_key']}
+          
+          options[:dependent] = case row['delete_action'].downcase
+          when 'cascade'
+            :delete
+          when 'set_null'
+            :nullify
+          else
+            nil
+          end
+          
           ForeignKeyDefinition.new(table_name, row['to_table'], options)
         end
       end
